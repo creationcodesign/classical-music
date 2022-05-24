54567f8fbbb773cb4877bfa020aec02e10362e53
Natalia <adelina09715@gmail.com>
2022-05-24 09:19:03 +0200

    data

---
+++ src/assets/data/data.js
@@ -0,0 +1,49 @@
+const composers = [
+    {
+        id: 1,
+        firstName: "Wolfgang Amadeus",
+        lastName: "Mozart",
+        birthPlace: "Salzburg, Austria",
+        birthDate: "27.01.1756",
+        music: "fantasia_in_minor_K397",
+        portrait: "mozart.jpg"
+    },
+    {
+        id: 2,
+        firstName: "Antonio",
+        lastName: "Vivaldi",
+        birthPlace: "Venedig, Italy",
+        birthDate: "04.03.1678",
+        music: "",
+        portrait: ""
+    },
+    {
+        id: 3,
+        firstName: "Pyotr Ilyich",
+        lastName: "Tchaikovski",
+        birthPlace: "Kamsko-Wotkinski Sawod, Russland",
+        birthDate: "07.05.1840",
+        music: "",
+        portrait: ""
+    },
+    {
+        id: 4,
+        firstName: "Niccolo",
+        lastName: "Paganini",
+        birthPlace: "Genua, Italy",
+        birthDate: "27.10.1782",
+        music: "",
+        portrait: ""
+    },
+    {
+        id: 5,
+        firstName: "Ludwig",
+        lastName: "Beethoven",
+        birthPlace: "Bonn, Germany",
+        birthDate: "16.12.1770",
+        music: "",
+        portrait: ""
+    }
+]
+
+export default composers
