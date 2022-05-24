c001a91a789420215be5ad9723b2a77eacbcd3c3
Natalia <adelina09715@gmail.com>
2022-05-24 21:19:18 +0200

    routes data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Composers from './components/Composers';
+import { BrowserRouter } from 'react-router-dom';
+import AllRoutes from './AllRoutes';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 import './styles/app.css';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Composers />
+      <BrowserRouter>
+        <Header />
+        <Navbar />
+        <AllRoutes />
+      </BrowserRouter>
     </div>
   );
 }
