80c322a9882f2b18ed38025413dd636fbba64ee3
Natalia <adelina09715@gmail.com>
2022-05-25 13:43:29 +0200

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import data from '../assets/data/data.js'
 
 export default function Home() {
+    const [composers] = useState(data)
+    const composer = composers.map(el => {
+        return (
+            <NavLink to={`/compositor/${el.lastName}`} className="content__link" key={el.id}>
+                <img src={require(`../assets/img/${el.portrait}`)} alt={el.lastName} className="composer-img" height={200} />
+                <h3>{el.lastName}</h3>
+            </NavLink>
+        )
+    })
+
     return (
-        <div>Home</div>
+        <div className='page home'>
+            <h2 className='home__title'>
+                {/* <span className='big'> 5 </span> */}
+                <br></br>
+                <span className='accent'>famouse composers</span>
+            </h2>
+            <div className='home__content'>
+                {composer}
+            </div>
+        </div>
     )
 }
